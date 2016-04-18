8b12717f5534e7cd60c605fd02506bdaf71f9de5
Mykola <mykola.lisivka@gmail.com>
2016-04-18 20:23:14 +0300

    Added method new to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,25 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.errors.empty?
+      redirect_to posts_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :autorId)
   end
 
 end
