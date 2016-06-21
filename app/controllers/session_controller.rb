e1fd08aada662d59fcd124482cef2203687bc2a3
Mykola <mykola.lisivka@gmail.com>
2016-06-21 17:01:30 +0300

    Redirect to previous page if you go to login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,12 +1,15 @@
 class SessionController < ApplicationController
+  rescue_from ActionController::RedirectBackError, with: :redirect_to_default
+  
   def new
+    session[:return_to] ||= request.referer
   end
 
   def create
     user = User.find_by_name(params[:name])
     if user
        session[:user_id] = user.id
-       redirect_to root_url, notice: 'Logged In!'
+       redirect_to session.delete(:return_to), notice: 'Logged In!'
      else
        render :new
     end
