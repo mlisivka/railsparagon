a5e37aa7f730d32ee5176e5dee37af991a56f446
Mykola <mykola.lisivka@gmail.com>
2016-07-04 17:26:02 +0300

    Established maximum number of players in the team and maximum number of teams to players

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,17 @@
 class Team < ActiveRecord::Base
-
-  has_and_belongs_to_many :users
+  
+  has_and_belongs_to_many :users, before_add: :team_limit
 
   has_many :invites
   has_and_belongs_to_many :tournaments
+  
+  private
+
+  def team_limit user
+    if user.teams.count >= 5
+      errors.add :user, "This player has too many teams, maximum: 5"
+      raise ActiveRecord::Rollback
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ActiveRecord::Base
 
   has_many :posts
-  has_and_belongs_to_many :teams
+  has_and_belongs_to_many :teams, before_add: :player_limit
 
   has_many :invitions,    class_name: "Invite", foreign_key: "recipient_id"
   has_many :send_invites, class_name: "Invite", foreign_key: "sender_id"
+  
+  private
+
+  def player_limit team
+    if team.users.count >= 5
+      errors.add :team, "This team has too many players, maximum: 5"
+      raise ActiveRecord::Rollback
+    end
+  end
 
 end
