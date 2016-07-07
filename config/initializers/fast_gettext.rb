fa317b00b2a308a286b5e8e164594aa6c15c0bf1
Mykola <mykola.lisivka@gmail.com>
2016-07-07 14:47:22 +0300

    Added translate on the ukrainian language

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,4 +1,3 @@
-FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
+FastGettext.add_text_domain 'app', path: 'locale', type: :mo, ignore_fuzzy: true
 FastGettext.default_available_locales = ['en','uk','ru'] #all you want to allow
 FastGettext.default_text_domain = 'app'
-FastGettext.locale = 'en'
