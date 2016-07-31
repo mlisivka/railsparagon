a2f35b06158cbc1406cdffef569ff46212076413
Mykola <mykola.lisivka@gmail.com>
2016-07-31 18:30:04 +0300

    Rspec: added test for Tournament

--- spec/factories/tournaments.rb
+++ spec/factories/tournaments.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
-  factory :tournamet do
-    title "The International"
+  factory :tournament do
+    sequence(:title) { |i| "The International #{i}" }
     max_team 16
     sequence(:tournament_begins) { |n| Time.now + n.weeks + 1.hour }
   end
---
+++ spec/model/tournament_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Tournament do
+  
+  let(:tournament) { build(:tournament) }
+  let(:teams)      { create_list(:team, 3) }
+  let(:team)       { create(:team) }
+  
+  describe ".before_add" do
+
+    it '#team_dublicate' do
+      tournament.teams << team
+      expect{ tournament.teams. << team }.to raise_exception ActiveRecord::Rollback
+    end
+   
+    it '#team_limit' do
+      tournament = build(:tournament, max_team: 2)
+      expect{ tournament.teams << teams }.to raise_exception ActiveRecord::Rollback
+    end
+    
+  end
+  
+end
