46359f863219d2acb278042bd9d36a2b1f390178
Mykola <mykola.lisivka@gmail.com>
2016-08-13 14:02:42 +0300

    Redirect to login if user visited /profile and is not logged

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = !params[:name].nil? ? User.where('lower(name) = ?', params[:name].downcase).first : current_user
+    if user_signed_in?
+      @user = !params[:name].nil? ? User.where('lower(name) = ?', params[:name].downcase).first : current_user
+      @teams = current_user.teams
+    else
+      unless params[:name].nil?
+        @user = User.where('lower(name) = ?', params[:name].downcase).first
+      else
+        redirect_to login_path and return
+      end
+    end
     render_404 unless @user
-    @teams = current_user.teams if user_signed_in?
   end
 
 end
